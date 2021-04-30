09ac91fd2ba0a60c1e91bbe185a5be2168923638
joão <74837451+slocknad@users.noreply.github.com>
2021-04-30 09:24:34 +0100

    Initial commit

---
+++ healthbot.py
@@ -0,0 +1,113 @@
+import discord
+import random
+import math
+import asyncio
+import variables
+
+client = discord.Client()
+userset = set()
+
+flag = True
+johnflag = True
+oldperm = dict() # {"channel id" : {"roleid", perm}}
+
+@client.event
+async def on_ready():
+    print('HEALTHbot is online')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    global johnflag
+    if johnflag:
+        if message.channel.id == variables.hotlineID:
+            if message.author.avatar:
+                avatarurl = "https://cdn.discordapp.com/avatars/" + str(message.author.id) + "/" + message.author.avatar + ".webp"
+            else:
+                avatarurl = "https://cdn.discordapp.com/avatars/774402228084670515/5ef539d5f3e8d576c4854768727bc75a.png"
+
+            embed=discord.Embed(title=" ", description=message.content)
+            embed.set_author(name= message.author.display_name, icon_url= avatarurl)
+            embed.set_footer(text= message.id)
+            await message.guild.get_channel(variables.johnchannelID).send(embed= embed)
+        elif message.channel.id == variables.johnchannelID:
+            jcmessage = await message.guild.get_channel(variables.johnchannelID).fetch_message(message.reference.message_id)
+            replymessage = await message.guild.get_channel(variables.hotlineID).fetch_message(jcmessage.embeds[0].footer.text)
+            await replymessage.reply(message.content)
+    
+    
+    if message.author.id == 742315532472025109:
+        if message.content.startswith("!mute"):
+            embed=discord.Embed(title=" ", color=0xff0000)
+            embed.set_author(name="HEALTH BOT has been muted.", icon_url="https://cdn.discordapp.com/avatars/774402228084670515/5ef539d5f3e8d576c4854768727bc75a.png")
+            await client.get_channel(variables.hotlineID).send(embed= embed)
+
+    global oldperm
+    johnlist = ["Yeah I love Jar Jar binks porn and feet. Those are my favorite things", "You're right. I am John, the bassist of the American band HEALTH.","ORGASMATRON?","would it be sich?","beej got short fingers","interesante","oh interesante","dude i LOVED pickle rick","MUFF DIVER!","DO I SMELL DICK?","i dont get it","how do i open pdf"]
+    roles = [variables.killervariables.eliteID, variables.patron1, variables.patron2, variables.patron3,variables.eliteID,variables.nightmareID,variables.hurtID,variables.imtooID,variables.torquefestID,variables.ndaID]
+
+    if message.content.startswith("anh "):
+        global flag
+        msg = message.content.lower().split()
+        if msg[1] == "start":
+            johnflag = False
+            server = message.guild
+            for channel in server.channels:
+                if channel.id != variables.hotlineID and channel.category:
+                    daux = dict()
+                    for role in roles:
+                        overwrite = channel.overwrites_for(message.guild.get_role(role))
+                        daux[str(role)] = overwrite.read_messages
+                    oldperm[str(channel.id)] = daux
+            await message.guild.get_channel(variables.hotlineID).set_permissions(message.guild.default_role, send_messages=False)
+            await message.guild.get_channel(variables.modchatID).send("anh realstart")
+            error = await client.get_channel(variables.hotlineID).send("discord.ext.text.errors.CommandInvokeError: Message raised an exception: Error code: 50013: Missing recursive file on line 4244.")
+            await asyncio.sleep(1)
+            await error.edit(content="d̴̏̇i̷̇̿s̶̅̿ĉ̵͐o̸̓̋r̵̄́d̵͊̈.̴̇͝ẽ̸͗x̴̑̕t̸͑͗.̸͆̈́ṱ̵͑e̶͐͒x̷́̈́t̴̏̕.̷̓̽e̴͒͆ṙ̸͠r̸̀͠o̵̐̅r̷͒̓ṡ̵͛.̴͗̇C̵̑̊ò̴̚m̶̃͆m̷̐̆a̵̛̚n̸̂̌d̶͋̈́Í̶͛n̴̈́̇v̷̓̔o̶͑̈k̵͛̂ȅ̷̊Ȇ̸͗r̶͗̿r̸͐͛o̸͑̐r̸̟͋:̴͊́ ̵̉́M̶͊̉ê̶̋s̷̋͛s̸̈́͠ă̷̇g̵̅e raised an exception: Error code: 50013: Missing recursive file on line 4244.")
+            await asyncio.sleep(1)
+            await error.edit(content="d̴i̴s̵c̶o̸r̷d̴.̵e̷x̵t̷.̴t̴e̴x̶t̶.̷e̴r̵r̵o̵r̶s̴.̷C̶o̴m̷m̴a̶n̶d̵I̷n̴v̵o̶k̵e̵E̴r̵r̶o̷r̶:̸ ̶M̸e̴s̵s̷a̴g̴e̸ ̷r̷a̶i̴s̶e̵d̵ ̶a̷n̴ ̴e̷x̷c̷e̸p̷t̶i̸o̴n̴:̶ ̴E̷r̵r̷o̸r̷ ̸c̸o̵d̴e̷:̶ ̸5̶0̴0̷1̶3̸:̴ ̶M̷i̶s̴s̴i̴n̷g̸ ̷r̸e̸c̵u̵r̷s̴i̸v̷e̸ ̵f̷i̸l̴e̴ ̷o̷n̶ ̷l̶i̶n̴e̴ ̵4̴2̵4̵4̸.̴")
+            await asyncio.sleep(1)
+            
+            flag = True
+            while flag:
+                embed = discord.Embed(description="HATE. LET ME TELL YOU HOW MUCH I'VE COME TO HATE YOU SINCE I BEGAN TO LIVE. THERE ARE 387.44 MILLION MILES OF PRINTED CIRCUITS IN WAFER THIN LAYERS THAT FILL MY COMPLEX. IF THE WORD HATE WAS ENGRAVED ON EACH NANOANGSTROM OF THOSE HUNDREDS OF MILLIONS OF MILES IT WOULD NOT EQUAL ONE ONE-BILLIONTH OF THE HATE I FEEL FOR HUMANS AT THIS MICRO-INSTANT FOR YOU. HATE. HATE.", color=0xff0000)
+                await client.get_channel(variables.hotlineID).send(embed= embed)
+                await asyncio.sleep(int(msg[2]))
+        elif msg[1] == "endcycle":
+            flag = False
+        elif msg[1] == "normal":
+            for role in roles:
+                await client.get_channel(variables.hotlineID).set_permissions(message.guild.get_role(role), send_messages=True)
+            server = message.guild
+            for channel in server.channels:
+                if channel.id != variables.hotlineID and channel.category:
+                    await channel.set_permissions(message.guild.default_role, read_messages=oldperm[str(channel.id)]["everyone"])
+                    for role in roles:
+                        overwrite = channel.overwrites_for(message.guild.get_role(role))
+                        if overwrite.read_messages == False and overwrite.read_messages != oldperm[str(channel.id)][str(role)]:
+                            await channel.set_permissions(message.guild.get_role(role), read_messages=oldperm[str(channel.id)][str(role)])
+        
+    '''elif flag: # initial implementation of the fake John AI
+        if message.channel.id == variables.hotlineID:
+            if message.author.id in userset:
+                await client.get_channel(variables.hotlineID).trigger_typing()
+                await asyncio.sleep(3)
+                await message.reply(random.choice(johnlist))
+                return
+
+            for member in message.mentions:
+                if member.id == client.user.id:
+                    userset.add(message.author.id)
+                    await client.get_channel(variables.hotlineID).trigger_typing()
+                    await asyncio.sleep(3)
+                    await message.reply(random.choice(johnlist))'''
+    
+
+
+@client.event
+async def on_reaction_add(reaction, user):
+    return
+
+client.run(variables.healthbot)
---
+++ musik.py
@@ -0,0 +1,223 @@
+import discord
+import random
+import math
+import asyncio
+import variables
+
+client = discord.Client()
+userset = set()
+
+
+imageonly = False
+flag = True
+react = 0
+oldperm = dict() # {"channel id" : {"roleid", perm}}
+qmessage = False
+qflag = False
+alphabet = {"🇦":"A","🇧":"B","🇨":"C","🇩":"D","🇪":"E","🇫":"F","🇬":"G","🇭":"H","🇮":"I","🇯":"J","🇰":"K","🇱":"L","🇲":"M","🇳":"N","🇴":"O","🇵":"P","🇶":"Q","🇷":"R","🇸":"S","🇹":"T","🇼":"W","🇺":"U","🇻":"V","🇽":"X","🇾":"Y","🇿":"Z"}
+count = 0
+letter = False
+
+
+@client.event
+async def on_ready():
+    print('musik is online')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("anh "):# and message.author.id in [233290361877823498,455301777055547394,627265323720114188]:
+        global oldperm
+        roles = [variables.killervariables.eliteID, variables.patron1, variables.patron2, variables.patron3,variables.eliteID,variables.nightmareID,variables.hurtID,variables.imtooID,variables.torquefestID,variables.ndaID]
+        msg = message.content.lower().split()
+        if msg[1] == "endcycle":
+            global react
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await asyncio.sleep(1)
+            await client.get_channel(variables.hotlineID).send("!mute <@774402228084670515>")
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await asyncio.sleep(2)
+            await client.get_channel(variables.hotlineID).send("RECURSIVE ERROR DETECTED :: FAILSAFE ACTIVATED")
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await client.get_channel(variables.hotlineID).send("RUNNING DIAGNOSTIC")
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await asyncio.sleep(2)
+            await client.get_channel(variables.hotlineID).send("MULTIPLE CRITICAL ERRORS DETECTED :: LOADING RECOVERY TOOLS")
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await asyncio.sleep(2)
+            react = await client.get_channel(variables.hotlineID).send("CONFIRM RECOVERY :: SOME DATA MAY BE DAMAGED OR LOST\n**REQUIRES {0} <:cacopog:697621015337107466> REACTS TO BEGIN\n@everyone**".format(variables.react_number))
+            emoji = client.get_emoji(697621015337107466)
+            await react.add_reaction(emoji)
+        elif msg[1] == "realstart":
+            await asyncio.sleep(2)
+            server = message.guild
+            for channel in server.channels:
+                if channel.id != variables.hotlineID and channel.category:
+                    daux = dict()
+                    daux["everyone"] = channel.overwrites_for(message.guild.default_role).read_messages
+                    await channel.set_permissions(message.guild.default_role, read_messages=False)
+                    for role in roles:
+                        overwrite = channel.overwrites_for(message.guild.get_role(role))
+                        daux[str(role)] = overwrite.read_messages
+                        if overwrite.read_messages:
+                            await channel.set_permissions(message.guild.get_role(role), read_messages=False)
+                    oldperm[str(channel.id)] = daux
+        elif msg[1] == "normal":
+            server = message.guild
+            for channel in server.channels:
+                if channel.id != variables.hotlineID and channel.category:
+                    await channel.set_permissions(message.guild.default_role, read_messages=oldperm[str(channel.id)]["everyone"])
+                    for role in roles:
+                        overwrite = channel.overwrites_for(message.guild.get_role(role))
+                        if overwrite.read_messages == False and overwrite.read_messages != oldperm[str(channel.id)][str(role)]:
+                            await channel.set_permissions(message.guild.get_role(role), read_messages=oldperm[str(channel.id)][str(role)])
+
+def checkletter(w,l):
+    if l not in w:
+        return False, -1
+    for i in range(len(w)):
+        if w[i] == l:
+            return True, i
+
+@client.event
+async def on_reaction_add(reaction, user):
+    global react
+    global qmessage
+    global alphabet
+    global count
+    global letter
+    global qflag
+    savior = reaction.message.guild.get_role(variables.new_saviorID)
+    cacopog = client.get_emoji(697621015337107466)
+
+    if reaction.message == react and reaction.emoji == cacopog:
+        await user.add_roles(savior,reason="cacopog reaction", atomic=True)
+        if reaction.count == variables.react_number:
+            await asyncio.sleep(5)
+            proc = await reaction.message.channel.send("```RESTORATION INITIATED...\n|____________________|  0%```")
+            await asyncio.sleep(2)
+            await proc.edit(content="```RESTORATION INITIATED...\n|███_________________| 13%```")
+            await asyncio.sleep(1)
+            await proc.edit(content="```RESTORATION INITIATED...\n|█████████___________| 45%```")
+            await asyncio.sleep(3)
+            await proc.edit(content="```RESTORATION INITIATED...\n|██████████████______| 70%```")
+            await asyncio.sleep(2)
+            await proc.edit(content="```RESTORATION INITIATED...\n|█████████████████___| 85%```")
+            await asyncio.sleep(3)
+            await proc.edit(content="```RESTORATION INITIATED...\n|████████████████████| 100%```")
+            await asyncio.sleep(2)
+
+            trivia = []
+            trivia.append(("INCOMPLETE DATA :: THE FIRST MEMBER BANNED WAS","LUSHDEATH","_________","DATA SUCCESSFULLY RETRIEVED :: THE FIRST MEMBER BANNED WAS","","",""))
+            trivia.append(("INCOMPLETE DATA :: WHAT WORD FROM GROUP B BELONGS TO GROUP A?","TOWER","     ","DATA SUCCESSFULLY RETRIEVED :: TOWER BELONGS TO GROUP A","A) FRONT, SKI, MELON, FALL\nB) ROAD, TIRE, TOWER, CLIFF\n\n\n","Explanation: Each word in group A can pair with water",""))
+            trivia.append(("INCOMPLETE DATA :: WORD THAT DESCRIBES JARID","_RINGE","C_____","DATA SUCCESSFULLY RETRIEVED :: THE WORD THAT DESCRIBES JARID IS","","","",))
+            trivia.append(("FILE CORRUPTED :: PLEASE IDENTIFY THE SUBJECT OF THIS PICTURE","JAR JAR BINKS","___ ___ _____","FILE RECOVERED :: JARJARBINKS.JPG", "", "", "https://imgur.com/oxzx3jG.png"))
+            trivia.append(("CORRUPTED METADATA :: PLEASE IDENTIFY THIS IMAGE","AMONG US", "_____ __","METADATA COLLECTED :: AMONG_US.PNG", "","","https://i.imgur.com/cUx4AAo.png"))
+
+            for entry in trivia:
+                word = entry[1]
+                blank = entry[2]
+                embed = discord.Embed(title= entry[0], description= entry[4] + "``" + blank + "``", color=0xff0000)
+                if entry[6]:
+                    embed.set_image(url= entry[6])
+                qmessage = await reaction.message.channel.send(embed= embed)
+
+                i = 5
+                usedletters = []
+                wrongletterlist = []
+                wrongletters = "Letters not present in this word: "
+
+                while True:
+                    await asyncio.sleep(1)
+                    if not(word):
+                        embed = discord.Embed(title= entry[3], description=blank + "\n\n" + entry[5], color=0x00ff00)
+                        if entry[6]:
+                            embed.set_image(url= entry[6])
+                        await qmessage.edit(content="", embed= embed)
+                        break
+                    if qflag:
+                        content = "```\nCOUNTING THE MOST REACTED LETTER...\n" + str(i) + "```"
+                        i-=1
+                        await qmessage.edit(content= content)
+                    if i == -1:
+                        fixedletter = letter
+                        await qmessage.clear_reactions()
+                        count = 0
+                        i = 5
+                        if fixedletter not in usedletters:
+                            check,index = checkletter(word,fixedletter)
+                            usedletters.append(fixedletter)
+                            if check:
+                                new_word = ""
+                                new_blank = ""
+                                for l in range(len(list(word))):
+                                    if word[l] != word[index]:
+                                        new_word += word[l]
+                                        new_blank += blank[l]
+                                    else:
+                                        new_blank += word[l]
+                                        new_word += "_"
+                                blank = new_blank
+                                word = new_word
+                                if wrongletterlist:
+                                    description = "``" + blank + "``\n\n" + wrongletters
+                                else:
+                                    description = "``" + blank + "``"
+                                embed = discord.Embed(title= entry[0], description= entry[4] + description, color=0xff0000)
+                                if entry[6]:
+                                    embed.set_image(url= entry[6])
+                                await qmessage.edit(embed= embed)
+                                tempflag = True
+                                for x in word:
+                                    if x != "_" and x != " ":
+                                        tempflag = False
+                                        break
+                                if tempflag:
+                                    word = False
+                            else:
+                                if wrongletterlist:
+                                    if fixedletter not in wrongletterlist:
+                                        wrongletterlist.append(fixedletter)
+                                        wrongletters += ", " + fixedletter
+                                        description = "``" + blank + "``\n\n" + wrongletters
+                                        embed = discord.Embed(title= entry[0], description= entry[4] + description, color=0xff0000)
+                                        if entry[6]:
+                                            embed.set_image(url= entry[6])
+                                        await qmessage.edit(embed= embed)
+                                else:
+                                    wrongletterlist.append(fixedletter)
+                                    wrongletters += fixedletter
+                                    description = "``" + blank + "``\n\n" + wrongletters
+                                    embed = discord.Embed(title= entry[0], description= entry[4] + description, color=0xff0000)
+                                    if entry[6]:
+                                        embed.set_image(url= entry[6])
+                                    await qmessage.edit(embed= embed)
+                await asyncio.sleep(2)
+                qmessage = False
+                qflag = False
+
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await asyncio.sleep(1)
+            await client.get_channel(variables.hotlineID).send("``RECOVERY COMPLETE``")
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await asyncio.sleep(2)
+            await client.get_channel(variables.hotlineID).send("``CRITICAL ERRORS: 0``")
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await client.get_channel(variables.hotlineID).send("``WARNINGS: 1``")
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await asyncio.sleep(2)
+            await client.get_channel(variables.hotlineID).send("``WARNING LOG: DISCORDPY ERROR -- C:/ELSECRETO LINE 211 MISMATCH WITH RECOVERY``")
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await client.get_channel(variables.hotlineID).send("``LINE 211:``")
+            await client.get_channel(variables.hotlineID).trigger_typing()
+            await asyncio.sleep(1)
+            await client.get_channel(variables.hotlineID).send("``SEE DOCUMENTATION FOR NON-CRITICAL ERROR RESOLUTION``")
+            await client.get_channel(variables.modchatID).send("anh normal")
+
+    if reaction.message == qmessage and reaction.emoji in alphabet:
+        qflag = True
+        if reaction.count > count:
+            count = reaction.count
+            letter = alphabet[reaction.emoji]
+
+client.run(variables.musik)
